c56c5eb38aae5f06db52505c6d7b2613499b0a56
Aakash <85379388+Aakash0332@users.noreply.github.com>
2023-04-25 18:15:25 +0530

    Update ex8 NAÏVE BAYES CLASSIFIER.py

--- ex8 NAÏVE BAYES CLASSIFIER.py
+++ ex8 NAÏVE BAYES CLASSIFIER.py
@@ -1,20 +1,13 @@
-import numpy as np
-import pandas as pd
-# load the iris dataset
 from sklearn.datasets import load_iris
-iris = load_iris()
-print(iris)
-X = iris.data
-y = iris.target
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-test_size=0.4, random_state=1)
 from sklearn.naive_bayes import GaussianNB
-gnb = GaussianNB()
-gnb.fit(X_train, y_train)
-y_pred = gnb.predict(X_test)
 from sklearn import metrics
-print("\nGaussian Naive Bayes model accuracy(in %):",
-metrics.accuracy_score(y_test, y_pred)*100)
-print("\nX",X)
-print("\ny",y)
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+gnb = GaussianNB().fit(X_train, y_train)
+accuracy = metrics.accuracy_score(y_test, gnb.predict(X_test)) * 100
+
+print(f'Gaussian Naive Bayes model accuracy (in %): {accuracy:.2f}')
+print(X)
+print(y)

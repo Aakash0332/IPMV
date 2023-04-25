570213c69b18625aad814c2f155602dcc122c5c4
Aakash <85379388+Aakash0332@users.noreply.github.com>
2023-04-25 18:12:13 +0530

    Update ex2 Histogram.py

--- ex2 Histogram.py
+++ ex2 Histogram.py
@@ -2,29 +2,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-image = cv2.imread("image.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("a",gray)
+image = cv2.imread("img1.png", cv2.IMREAD_GRAYSCALE)
+equ = cv2.equalizeHist(image)
 
-hist,bins=np.histogram(gray.flatten(),256,[0,256])
-cdf = hist.cumsum()
-cdf_normalized = cdf*float(hist.max())/cdf.max()
-plt.plot(cdf_normalized,color='b')
-plt.hist(gray.flatten(),256,[0,256],color='r')
-plt.xlim([0,256])
-plt.legend(('cdf','histogram'),loc='upper left')
+plt.hist(image.ravel(), 256, [0, 256])
 plt.show()
 
-equ=cv2.equalizeHist(gray)
-cv2.imshow("a",equ)
-
-hist,bins=np.histogram(equ.flatten(),256,[0,256])
-cdf=hist.cumsum()
-cd_normalized = cdf*float(hist.max())/cdf.max()
-plt.plot(cdf_normalized,color='b')
-plt.hist(equ.flatten(),256,[0,256],color='r')
-plt.xlim([0,256])
-plt.legend(('cdf','histogram'),loc='upper left')
+plt.hist(equ.ravel(), 256, [0, 256])
 plt.show()
-
-cv2.waitKey(5000)
